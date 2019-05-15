d29a36a2c9b739387b9cc105e90a54c8d1af37d0
Jeff Chung <chungchi300@hotmail.com>
2019-05-15 09:43:19 +0800

    push

--- 365.water-and-jug-problem.js
+++ 365.water-and-jug-problem.js
@@ -3,9 +3,19 @@
 If z liters of water is measurable, you must have z liters of water contained within one or both buckets by the end.
 
 */
+
+/* Previous
+
+https://zh-yue.wikipedia.org/wiki/%E6%9C%80%E5%A4%A7%E5%85%AC%E5%9B%A0%E6%95%B8
+
+https://zh-yue.wikipedia.org/wiki/%E8%BC%BE%E8%BD%89%E7%9B%B8%E9%99%A4%E6%B3%95
+
+*/
+
 /*
 https://zh.wikipedia.org/wiki/%E8%B2%9D%E7%A5%96%E7%AD%89%E5%BC%8F
 https://en.wikipedia.org/wiki/Euclidean_algorithm#Algorithmic_efficiency
+
 */
 
 /*
